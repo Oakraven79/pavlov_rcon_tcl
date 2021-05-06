3eaa1094b7ab4c86f9181d3d236c9fc944d67223
Oakraven79 <github@clan-munro.com>
2021-05-06 16:11:17 +1000

    Formatting the mock rcon commands

--- pavlov_rcon_tcl/rcon_connector.py
+++ pavlov_rcon_tcl/rcon_connector.py
@@ -54,55 +54,54 @@ def test_replies(command):
     :return:
     """
     if command == "RefreshList":
-            if random.randint(0,10) > 4:
+            if random.randint(0,10) > -1:
                 data = {
-                        "PlayerList": [
-                                {
-                                        "Username": "Oakraven",
-                                        "UniqueId": "76561198040783597"
-                                },
-                            {
-                                "Username": "Oakraven2",
-                                "UniqueId": "76561198040783598"
-                            },
-                            {
-                                "Username": "Oakraven3",
-                                "UniqueId": "76561198040783599"
-                            },
-                            {
-                                "Username": "Oakraven4",
-                                "UniqueId": "76561198040783510"
-                            },
-                            {
-                                "Username": "Oakraven5",
-                                "UniqueId": "76561198040783511"
-                            },
-                            {
-                                "Username": "Oakraven6",
-                                "UniqueId": "76561198040783512"
-                            },
-                            {
-                                "Username": "Oakraven7",
-                                "UniqueId": "76561198040783513"
-                            },
-                            {
-                                "Username": "Oakraven8",
-                                "UniqueId": "76561198040783517"
-                            },
-                            {
-                                "Username": "Oakraven9",
-                                "UniqueId": "76561198040783514"
-                            },
-                            {
-                                "Username": "Oakraven10",
-                                "UniqueId": "76561198040783515"
-                            },
-                            {
-                                "Username": "Oakraven11",
-                                "UniqueId": "76561198040783516"
-                            },
-
-                        ]
+                    "PlayerList": [
+                        {
+                            "Username": "Oakraven",
+                            "UniqueId": "76561198040783597"
+                        },
+                        {
+                            "Username": "Oakraven2",
+                            "UniqueId": "76561198040783598"
+                        },
+                        {
+                            "Username": "Oakraven3",
+                            "UniqueId": "76561198040783599"
+                        },
+                        {
+                            "Username": "Oakraven4",
+                            "UniqueId": "76561198040783510"
+                        },
+                        {
+                            "Username": "Oakraven5",
+                            "UniqueId": "76561198040783511"
+                        },
+                        {
+                            "Username": "Oakraven6",
+                            "UniqueId": "76561198040783512"
+                        },
+                        {
+                            "Username": "Oakraven7",
+                            "UniqueId": "76561198040783513"
+                        },
+                        {
+                            "Username": "Oakraven8",
+                            "UniqueId": "76561198040783517"
+                        },
+                        {
+                            "Username": "Oakraven9",
+                            "UniqueId": "76561198040783514"
+                        },
+                        {
+                            "Username": "Oakraven10",
+                            "UniqueId": "76561198040783515"
+                        },
+                        {
+                            "Username": "Oakraven11",
+                            "UniqueId": "76561198040783516"
+                        },
+                    ]
                 }
             else:
                 data = {
@@ -115,23 +114,21 @@ def test_replies(command):
                             "Username": "Oakraven2",
                             "UniqueId": "76561198040783598"
                         },
-
-
                     ]
                 }
     elif command.startswith("InspectPlayer"):
         unique_id = command.split(" ")[1]
 
         data = {
-                        "PlayerInfo":
-                        {
-                                "PlayerName": "Oakraven{}".format(unique_id[-4:]),
-                                "UniqueId": "{}".format(unique_id),
-                                "KDA": "0/4/0",
-                                "Score": "0",
-                                "Cash": "9400",
-                                "TeamId": "0"
-                        }
+                    "PlayerInfo":
+                    {
+                        "PlayerName": "Oakraven{}".format(unique_id[-4:]),
+                        "UniqueId": "{}".format(unique_id),
+                        "KDA": "0/4/0",
+                        "Score": "0",
+                        "Cash": "9400",
+                        "TeamId": "0"
+                    }
                 }
     elif command == "ItemList":
         data = {'ItemList': ['ak', 'vanas', 'AUG', 'awp', 'smg', 'shotgun', 'AR', 'sock', 'm9', 'cet9', 'Armour', 'kevlarhelmet', 'Grenade', 'grenade_ru', 'AK47', 'AK12', 'DE', '1911', 'mp5', 'p90', 'Smoke', 'smoke_ru', 'flash', 'flash_ru', 'sawedoff', 'Pliers', 'LMGA', 'AutoShotgun', 'AntiTank', 'kar98', 'AutoSniper', '57', 'uzi', 'Knife', 'DrumShotgun', 'Revolver', 'supp_pistol', 'supp_rifle', 'scope', 'Grip_Angled', 'Grip_Vertical', 'acog', 'holo', 'reddot', 'painkillers', 'ammo_rifle', 'ammo_sniper', 'ammo_smg', 'ammo_pistol', 'ammo_shotgun', 'ammo_special', 'taser', 'crowbar', 'boltcutters', 'lockpick', 'handcuffs', 'repairtool', 'pickaxe', 'keycard', 'syringe', 'luger', 'mp40', 'G43', 'Tokarev', 'webley', 'm1garand', 'svt40', 'grenade_us', 'smoke_us', 'grenade_ger', 'smoke_ger', 'grenade_svt', 'smoke_svt', 'bar', 'bren', 'ppsh', 'sten', 'mosin', 'springfield', 'thompson', 'mg42', 'leeenfield', 'RL_M1A1', 'RL_PIAT', 'rl_panzer', 'stg44', 'dp27', 'kross', 'vss', 'tankmg', 'tankturret', 'backblast', 'runover', 'Fire', 'fall', 'scar', 'skinhelmet_us', 'skinhelmet_ger', 'skinhelmet_svt', 'FlashLight', 'Katana', 'AdminSword', 'DiamondSword', 'OneHandedSword', 'BlueRoseSword', 'ContributorSword', 'ContributorSwordSecond', 'ContributorSwordThird', 'ContributorSwordFour', 'ContributorSwordFifth', 'Torch', 'AncientSWORD', 'TwitchSword', 'DarkAncientSword', 'NetherScythe', 'PerkBottleBase', 'juggernautbottle', 'staminupbottle', 'ExcaliburSword', 'TestingBow', 'MCBow', 'PatreonBow', 'Enderpearl', 'SnakeBow', 'PatreonSword', 'DiamondAxe']}
